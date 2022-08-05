40922bcfbc5035856c32c521e59dfde4584078f3
Tsche <Tsche@users.noreply.github.com>
2022-08-05 02:51:08 +0200

    fix cmake rule

--- capnp.cmake
+++ capnp.cmake
@@ -1,7 +1,8 @@
-capnp_generate_cpp(palliate_proto palliate_proto_headers 
+capnp_generate_cpp(palliate_proto palliate_proto_headers
   ${CMAKE_CURRENT_LIST_DIR}/app.capnp
   ${CMAKE_CURRENT_LIST_DIR}/ui.capnp
   ${CMAKE_CURRENT_LIST_DIR}/input.capnp
   ${CMAKE_CURRENT_LIST_DIR}/debug.capnp
   ${CMAKE_CURRENT_LIST_DIR}/data_channel.capnp
+  ${CMAKE_CURRENT_LIST_DIR}/auth.capnp
 )
